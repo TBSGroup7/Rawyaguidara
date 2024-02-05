18716bd8e7e9f8ca5e26ce59dd4076f833f51771
Rawyaguidara <guidararawya2003@gmail.com>
2024-02-05 15:48:59 +0100

    W2 My first React component

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+/* import * as REACT from "react";
+function App() {
+  const title="React";
+  return(
+    <div>
+      <h1>Hello {title}</h1>
+      <label htmlFor="search">Search:</label>
+      <input id="search" type="text" />
+    </div>
+  );
+}
+export default App; */
+
+/*import * as REACT from "react";
+const welcome = {
+  greeting: "Hey",
+  title: "React",
+};
+function App() {
+  return(
+    <div>
+      <h1> {welcome.greeting} {welcome.title} </h1>
+      <label htmlFor="search">search: </label>
+      <input id="search" type="text" />
+    </div>
+  );
+
+} 
+export default App;
+*/
+
+import * as REACT from "react";
+const welcome = {
+  greeting: "Hey",
+  title: "React",
+};
+function App() {
+  return(
+    <div>
+      <h1> {welcome.greeting} {welcome.title} </h1>
+      <label htmlFor="search">search: </label>
+      <input id="search" type="text" />
+    </div>
+  );
+}
+export default App;
